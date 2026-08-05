02f6d2cc1c0420b538f6618fd617d0ef829586db
beijing10000-boop <beijing10000-boop@users.noreply.github.com>
2026-08-04 23:43:52 -0700

    发布 v3.0.0-rc.6

--- deploy/Install-Online.ps1
+++ deploy/Install-Online.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string]$Version = "v3.0.0-rc.5",
+    [string]$Version = "v3.0.0-rc.6",
     [string]$Repository = "beijing10000-boop/folder2feishu-wiki",
     [string]$InstallDir = "D:\Folder2FeishuDrive\App",
     [string]$RuntimeDir = "D:\Folder2FeishuDrive\Data"
